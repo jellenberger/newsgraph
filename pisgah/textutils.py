6f9a4703e95006ca2f825e3426ee837637e34f77
Jim Ellenberger <jim.ellenberger@gmail.com>
2015-03-09 08:11:06 -0600

    Added cleanup and comments.

--- pisgah/textutils.py
+++ pisgah/textutils.py
@@ -31,16 +31,26 @@ commonpunc_transtable = {
 
 ## String simplification and normalization functions ##
 
+# convert all whitespace characters in a string to spaces
 def normspace(s):
     s = s.translate(space_transtable).strip()
     return s
 
+
+# remove all characters in a string that don't have ascii equivalents
+# (after performing some very basic translations) 
 def asciichars(s):
     s = unicodedata.normalize('NFKD', s) # normalize to combining chars
     s = s.translate(commonpunc_transtable) # punctuation to ascii
     s = s.encode('ascii', 'ignore').decode('ascii') # ascii only
     return s
 
+
+# attempt to parse a string representing a date and convert 
+# it to an iso 8601 format string
 def normdatestring(s):
     s = dateutil.parser.parse(s).isoformat()
     return s
+
+## TODO add way to reduce 2+ spaces in a row with one space
+## TODO add func to replace hyphen or underscore with space
