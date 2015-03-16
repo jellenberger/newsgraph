3ca0c4eaec3e5eb00eca5fb0c5857200869e3547
Jim Ellenberger <jim.ellenberger@gmail.com>
2015-03-16 07:55:36 -0600

    Added code to find similar tweets. Incorporated new text utilities.

--- pisgah/tweetprocesser.py
+++ pisgah/tweetprocesser.py
@@ -2,13 +2,20 @@ import sys
 import re
 import sqlite3
 from textblob import TextBlob
+from fuzzywuzzy import fuzz
 import config
+import textutils as tu
 import random # temp?
+from pprint import pprint #temp?
+
 
 
 def cleantweettext(twttext):
-    # delete urls
-    twttext = re.sub('(([\w-]+://?|www[.])[^\s()<>]+)', '', twttext)
+    twttext = tu.rmurls(twttext)
+    twttext = tu.puncspace(twttext)
+    twttext = twttext.replace('#', '')
+    twttext = twttext.replace('@', '')
+    twttext = tu.singlespaces(twttext).strip()
     return twttext
 
 
@@ -26,18 +33,46 @@ def retrievetweets():
         return tweetlist
 
 
-def tagtweets(twtlist):
+def maketweetbloblist(twtlist):
+    twtbloblist = []
     for twt in twtlist:
-        twttext = twt[3]
-        twtblob = TextBlob(cleantweettext(twttext))
-        print(twtblob.tags)
-        
+        twttextblob = TextBlob(cleantweettext(twt[3]))
+        twtdatetime = tu.stringtodate(twt[2])
+        twtbloblist.append( (twt[0], twt[1], twtdatetime, twttextblob) )
+    return twtbloblist
+
+
+def getsimilar(reftwtblob, twtbloblist):
+    simtwts = [reftwtblob]
+    for twtblob in twtbloblist:
+        if  ((reftwtblob[2] - twtblob[2]).total_seconds() <= 64800 and 
+             reftwtblob[1] != twtblob[1]):
+            ratio = fuzz.token_sort_ratio(reftwtblob[3].string, twtblob[3].string)
+            if ratio >= 75 and ratio < 90:
+                simtwts.append(twtblob)
+    return simtwts
+
+
 
 ## Main ##
 
 def main():
-    twtlist = random.sample(retrievetweets(), 5)
-    print(tagtweets(twtlist))
+    print('\n')
+    print('Retrieving tweets from database.\n')
+    twtbloblist = maketweetbloblist(retrievetweets())
+    print('Analyzing tweets.\n')
+    tweetnum = 1
+    percentunits = int(len(twtbloblist) / 100)
+    while twtbloblist:
+        if tweetnum % percentunits == 0:
+            print(tweetnum * 100 / len(twtbloblist), 'percent complete.\n')
+        reftwtblob = twtbloblist.pop()
+        simtwts = getsimilar(reftwtblob, twtbloblist)
+        if(len(simtwts) > 2):
+            pprint(simtwts)
+            print('\n')
+        tweetnum += 1
+
 
 
 if __name__ == "__main__":
