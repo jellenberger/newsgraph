5aa3b97b0fd0259ca361388cfccb6e30f9ed9910
Jim Ellenberger <jim.ellenberger@gmail.com>
2015-03-16 07:53:45 -0600

    Added additional text processing utiliites.

--- pisgah/textutils.py
+++ pisgah/textutils.py
@@ -1,3 +1,6 @@
+import sys
+import re
+import string
 import unicodedata
 import dateutil.parser
 from html import unescape
@@ -27,6 +30,9 @@ commonpunc_transtable = {
     ord('\u2014'): '-',
 }
 
+rmpunc_transtable = dict.fromkeys(i for i in range(sys.maxunicode)
+                                  if unicodedata.category(chr(i)).startswith('P'))
+rmpunc_transtable.update(dict.fromkeys(map(ord, string.punctuation)))
 
 
 ## String simplification and normalization functions ##
@@ -41,7 +47,7 @@ def normspace(s):
 # (after performing some very basic translations) 
 def asciichars(s):
     s = unicodedata.normalize('NFKD', s) # normalize to combining chars
-    s = s.translate(commonpunc_transtable) # punctuation to ascii
+    s = s.translate(commonpunc_transtable) # some punctuation to ascii analog 
     s = s.encode('ascii', 'ignore').decode('ascii') # ascii only
     return s
 
@@ -52,5 +58,33 @@ def normdatestring(s):
     s = dateutil.parser.parse(s).isoformat()
     return s
 
-## TODO add way to reduce 2+ spaces in a row with one space
-## TODO add func to replace hyphen or underscore with space
+
+# attempt to parse a string representing a date and return
+# a datetime value
+def stringtodate(s):
+    return dateutil.parser.parse(s)
+
+
+# reduces all sequences of space chars with a single space char
+def singlespaces(s):
+    s = re.sub(' +', ' ', s)
+    return s
+
+
+# removes all punctuation from string s
+def rmpunc(s):
+    s = s.translate(rmpunc_transtable)
+    return s
+
+
+# replaces space-like punctuation with a space
+def puncspace(s):
+    s = s.replace('-', ' ')
+    s = s.replace('_', ' ')
+    return s
+
+
+# remove url from string
+def rmurls(s):
+    s = re.sub('(([\w-]+://?|www[.])[^\s()<>]+)', '', s)
+    return s
