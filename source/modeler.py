d433234263b3fdc9e941f064186cf17e99e6241a
Jim Ellenberger <jim.ellenberger@gmail.com>
2015-05-03 09:54:58 -0600

    Read POS models from Reuters TRC2; refactor to process as generator

--- source/modeler.py
+++ source/modeler.py
@@ -2,38 +2,31 @@ import sys
 import re
 import random
 import pickle
+import gzip
+import csv
+import codecs
 from collections import Counter
 import nltk
 from nltk.util import ngrams
 
-from xml.etree.ElementTree import iterparse
-from html import unescape
+#from xml.etree.ElementTree import iterparse
+#from html import unescape
 import textutils as tu
 
 
 
-def clean_ns_headline(s):
-    s = re.sub(r'(<!--.*?-->|<[^>]*>)', '', s)
-    s = re.sub(r' #(\d+);', r'&#\1;', s)
-    s = s.replace(' nbsp;', '&nbsp;')
-    s = s.replace(' quot;', '&quot;')
-    s = s.replace(' amp;', '&amp;')
-    s = unescape(s)
-    s = s.replace('\\', '')
+def clean_headline(s):
+    s = re.sub(r'SERVICE ALERT ?-', '', s)
     s = tu.asciichars(s)
     s = s.replace('...', '')
     # TODO need same dash treatment as tweet processer?
     return s.lower().strip()
 
 
-def get_phrasetags(phrases):
-    phrasetags = []
-    for phrase in phrases:
-        taggedphrase = nltk.pos_tag(nltk.word_tokenize(phrase))
-        tags = tuple(t[1] for t in taggedphrase)
-        phrasetags.append(tags)
+def get_phrasetags(phrase):
+    taggedphrase = nltk.pos_tag(nltk.word_tokenize(phrase))
+    phrasetags = [token[1] for token in taggedphrase]
     return phrasetags
-
 
 def calc_ngramcounts(wordlists, n):
     ngramcounts = Counter() 
@@ -44,33 +37,25 @@ def calc_ngramcounts(wordlists, n):
 
 
 def read_headlines():
-    fname = 'data/newsspace200.xml'
-    doc = iterparse(fname)
-    headlines = []
-    for event, elem in doc:
-        if elem.tag == 'title':
-            headline = elem.text
-            if headline is not None:
-                headline = clean_ns_headline(headline)
-                headlines.append(headline)
-    return headlines
+    fname = 'data/headlines.csv.gz'
+    with gzip.open(fname, 'r') as f:
+        rdr = csv.reader(codecs.iterdecode(f, 'latin-1'))
+        for line in rdr:
+            if line:
+                yield line
 
 
 def main():
+    n = 4
+    verbtags = ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
     print('Reading headlines.')
-    headlines = read_headlines()[:5]
-    print(len(headlines), 'headlines read.')
-    print('Tagging headlines.')
-    headlinetags = get_phrasetags(headlines)
-    print(len(headlinetags), 'headlines tagged.')
-    print('Counting n-grams')
-    ngramcounts = calc_ngramcounts(headlinetags, 3)
-    #from pprint import pprint
-    #pprint(ngramcounts)
-    print('Counted', len(ngramcounts), 'unique n-grams.')
-    print(ngramcounts, type(ngramcounts), len(ngramcounts))
-    with open('data/posmodel.pickle', 'wb') as f:
-        pickle.dump(ngramcounts, f)
+    headline_reader = read_headlines()
+    for _ in range(100):
+        phrase = next(headline_reader)[1]
+        tags = get_phrasetags(phrase)
+        phraseverbs = [tag for tag in tags if tag in verbtags]
+        if phraseverbs and len(tags) >= 8:
+            print(phrase)
 
 
 if __name__ == "__main__":
