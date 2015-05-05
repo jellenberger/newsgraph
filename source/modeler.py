d0356a45e171bb4384fdb8bc1ada1b15d32cc19d
Jim Ellenberger <jim.ellenberger@gmail.com>
2015-05-05 07:07:38 -0600

    Add cleanup and processing code for TRC2 headlines

--- source/modeler.py
+++ source/modeler.py
@@ -8,35 +8,46 @@ import codecs
 from collections import Counter
 import nltk
 from nltk.util import ngrams
-
-#from xml.etree.ElementTree import iterparse
-#from html import unescape
 import textutils as tu
 
 
 
 def clean_headline(s):
-    s = re.sub(r'SERVICE ALERT ?-', '', s)
     s = tu.asciichars(s)
     s = s.replace('...', '')
+    s = re.sub(r'[-:] ?[\w.]+( [\w.]+){0,2}$', '', s)
+    #s = re.sub(r'(update|wrapup) ?\d* ?[-:] ?', '', s) # possible duplicates?
+    s = re.sub(r' ?(cricket|tennis|soccer|golf) ?-', '', s)
+    s = re.sub(r' +', ' ', s)
     # TODO need same dash treatment as tweet processer?
+    # TODO check for % of english words
     return s.lower().strip()
 
 
-def get_phrasetags(phrase):
-    taggedphrase = nltk.pos_tag(nltk.word_tokenize(phrase))
-    phrasetags = [token[1] for token in taggedphrase]
-    return phrasetags
+def isjunk(s):
+    if (
+            re.search(r'^[\w.]+( [\w.]+){0,2} ?[-:]', s) or
+            re.search(r'^\([\w.]+( [\w.]+){0,2} ?\)', s) or
+            re.search(r'<(\w+)?(\.\w+)?>', s) or
+            re.search(r'\([\w.]+;[\w.]+\)', s) or
+            re.search(r'-+ ?reuters', s) or
+            re.search(r'^ *[\*-]+', s) or
+            'daily earnings hits' in s or
+            'this is a test' in s or
+            'please ignore' in s or
+            'reuters world news' in s or
+            'reuters sports schedule' in s or
+            'quote of the day' in s or
+            'factors to watch' in s or
+            'margin intex for' in s
+    ):
+        return True
 
-def calc_ngramcounts(wordlists, n):
-    ngramcounts = Counter() 
-    for wordlist in wordlists:
-        for ngram in ngrams(wordlist, n):
-            ngramcounts[ngram] += 1
-    return ngramcounts
+    else:
+        return False
 
 
-def read_headlines():
+def headline_reader():
     fname = 'data/headlines.csv.gz'
     with gzip.open(fname, 'r') as f:
         rdr = csv.reader(codecs.iterdecode(f, 'latin-1'))
@@ -45,17 +56,43 @@ def read_headlines():
                 yield line
 
 
+def save_counter(counterobj, fname):
+    with open(fname, 'wb') as f:
+        pickle.dump(counterobj, f)
+
+
 def main():
+    linecount = 0
+    taggedcount = 0
     n = 4
-    verbtags = ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
-    print('Reading headlines.')
-    headline_reader = read_headlines()
-    for _ in range(100):
-        phrase = next(headline_reader)[1]
-        tags = get_phrasetags(phrase)
-        phraseverbs = [tag for tag in tags if tag in verbtags]
-        if phraseverbs and len(tags) >= 8:
-            print(phrase)
+    english_words = set(w.lower() for w in nltk.corpus.words.words())
+    verbtags = {'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ'}
+    ngramcounter = Counter()
+    reader = headline_reader()
+    print('Begin processing')
+    for line in reader:
+        linecount += 1
+        phrase = clean_headline(line[1].lower())
+        taggedphrase = nltk.pos_tag(nltk.word_tokenize(phrase))
+        tags = [token[1] for token in taggedphrase]
+        wordset = {token[0] for token in taggedphrase if token[0].isalpha()}
+        hasverb = any([tag in verbtags for tag in tags])
+        if (
+                hasverb and 
+                len(tags) >= 8 and 
+                not isjunk(phrase) and
+                len(wordset - english_words) < (len(wordset) / 2)
+        ):
+            tagngrams = list(ngrams(tags, n))
+            ngramcounter.update(tagngrams)
+            taggedcount += 1
+        if linecount % 10000 == 0:
+            print(linecount, 'lines processed;', taggedcount, "lines' POS added to model")
+    print('Done.')
+    print(linecount, 'lines processed;', taggedcount, "lines' POS added to model")
+    print(len(ngramcounter), 'unique ' + str(n) + '-grams')
+    print(ngramcounter.most_common(5))
+    save_counter(ngramcounter, 'data/' + str(n) + 'grammodel.pickle')
 
 
 if __name__ == "__main__":
