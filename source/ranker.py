6d6484fa952213be4d4ed459c48328bf235c41ef
Jim Ellenberger <jim.ellenberger@gmail.com>
2015-12-02 18:34:51 -0700

    begin adding n-gram grammatical model scoring

--- source/ranker.py
+++ source/ranker.py
@@ -1,7 +1,9 @@
 import sys
 import random
+import pickle
 import networkx as nx
 import grapher
+from nltk.util import ngrams
 
 
 ## Weighting ##
@@ -27,27 +29,42 @@ def main():
 
     verbtags = {'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ'}
 
+    with open('../data/4grammodel.pickle', 'rb') as f:
+        g_model = pickle.load(f)
+
     grpids = grapher.get_tweetgroupids()
     twtgrp = grapher.get_tweetgroup(random.choice(grpids))
     taggedphrases = [grapher.tag_phrase(twt[4].lower()) for twt in twtgrp]
     G = grapher.graph_taggedphrases(taggedphrases)
     G = grapher.weight_edges(G)
     paths = get_weightedpaths(G)
-    verbed_paths = list(filter(lambda path: any([t[1] in verbtags for t in path[0]]), paths))
-    sorted_paths = sorted(verbed_paths[:30], key=lambda x: (x[1], x[2]))
+    #verbed_paths = list(filter(lambda path: any([t[1] in verbtags for t in path[0]]), paths))
+    sorted_paths = sorted(paths[:30], key=lambda x: (x[1], x[2]))
     resorted_paths = sorted(sorted_paths, key=lambda x: (x[1]/x[2], x[2]))
 
     for phrase in taggedphrases:
         print(' '.join([word[0] for word in phrase]))
     print('')
 
-    for path in sorted_paths:
-        print(' '.join([tok[0] for tok in path[0]]))
-    print('')
+    # for path in sorted_paths[:2]:
+    #     print(' '.join([tok[0] for tok in path[0]]))
+    # print('')
 
-    for path in resorted_paths:
-        print(' '.join([tok[0] for tok in path[0]]))
-    print('')
+    # for path in resorted_paths[:2]:
+    #     print(' '.join([tok[0] for tok in path[0]]))
+    # print('')
+
+    for path in resorted_paths[:5]:
+        words = [tok[0] for tok in path[0]]
+        del words[0]
+        del words[-1]
+        pos_tags = [tok[1] for tok in path[0]]
+        del pos_tags[0]
+        del pos_tags[-1]
+        tag_ngrams = list(ngrams(pos_tags, 4))
+        g_model_score = sum([g_model[ngram] for ngram in tag_ngrams]) / len(tag_ngrams)
+        phrase = ' '.join(words)
+        print(g_model_score, ' ', phrase)
 
 
 if __name__ == "__main__":
